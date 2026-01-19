e8c0832fb32a0d3e186bb0bdd8f3154738801569
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-01-19 13:16:06 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.java
@@ -0,0 +1,34 @@
+// Last updated: 1/19/2026, 1:16:07 PM
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        // Push to minStack only if it's empty or new val is <= current min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int removed = stack.pop();
+        // If popped element is the minimum, remove it from minStack too
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
