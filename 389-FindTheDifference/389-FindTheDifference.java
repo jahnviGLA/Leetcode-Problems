05786ba2eb471652784c8b31caab53f979c8a224
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-01-19 13:15:40 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,13 @@
+// Last updated: 1/19/2026, 1:15:41 PM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int xor = 0;
+        for (char c : s.toCharArray()) {
+            xor ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            xor ^= c;
+        }
+        return (char) xor;
+    }
+}
