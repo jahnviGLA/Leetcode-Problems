5c5eb2418583fdb047a2a5b48b7de1679625dcb3
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-01-19 13:14:56 +0530

    Create 2089-MaximumMatrixSum

---
+++ 2089-MaximumMatrixSum/2089-MaximumMatrixSum.java
@@ -0,0 +1,20 @@
+// Last updated: 1/19/2026, 1:14:57 PM
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        long sum = 0;
+        int minAbs = Integer.MAX_VALUE;
+        int negativeCount = 0;
+        for (int[] row : matrix) {
+            for (int val : row) {
+                if (val < 0) negativeCount++;
+                int absVal = Math.abs(val);
+                sum += absVal;
+                minAbs = Math.min(minAbs, absVal);
+            }
+        }
+        if (negativeCount % 2 == 1) {
+            sum -= 2L * minAbs;
+        }
+        return sum;
+    }
+}
