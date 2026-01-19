602a90a318315ca5459ae36aeea6f3082bff5257
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-01-19 13:15:13 +0530

    Create 1001-NRepeatedElementInSize2nArray

---
+++ 1001-NRepeatedElementInSize2nArray/1001-NRepeatedElementInSize2nArray.java
@@ -0,0 +1,14 @@
+// Last updated: 1/19/2026, 1:15:14 PM
+import java.util.*;
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return num;
+            }
+            seen.add(num);
+        }
+        return -1; 
+    }
+}
