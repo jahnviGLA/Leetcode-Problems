8a254852afc818c3d160ca55b6fd47aa315d815b
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-01-19 13:16:48 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,18 @@
+// Last updated: 1/19/2026, 1:16:49 PM
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        int low = 1, high = x / 2;
+        int ans = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid <= x / mid) {
+                ans = mid;      
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
