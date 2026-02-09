d122d974b6bc7618b69d93eaf8079425c8ecb24a
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-02-09 22:43:49 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/version/java/455-AssignCookies_20260209_224348.java
@@ -0,0 +1,20 @@
+// Last updated: 2/9/2026, 10:43:48 PM
+1class Solution {
+2    public int findContentChildren(int[] g, int[] s) {
+3        Arrays.sort(g);
+4        Arrays.sort(s);
+5        int i = 0, j = 0;
+6        int count = 0;
+7        while (i < g.length && j < s.length) {
+8            if (s[j] >= g[i]) {
+9                count++;
+10                i++;
+11                j++;
+12            } else {
+13                j++;
+14            }
+15        }
+16        return count;
+17    }
+18}
+19
