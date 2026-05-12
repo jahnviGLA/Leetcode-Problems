9b4e2dad6f7bcc081a21cfb44d3a4057b8c93814
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-05-12 23:06:38 +0530

    Create 1665-MinimumInitialEnergytoFinishTasks

---
+++ 1665-MinimumInitialEnergytoFinishTasks/version/java/1665-MinimumInitialEnergytoFinishTasks_20260512_230645.java
@@ -0,0 +1,16 @@
+// Last updated: 5/12/2026, 11:06:45 PM
+1class Solution {
+2
+3    public int minimumEffort(int[][] tasks) {
+4        Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
+5        int ans = 0;
+6        int remain = 0;
+7        for (int[] task : tasks) {
+8            if (remain <= task[1]) {
+9                ans += task[1] - remain;
+10            }
+11            remain = Math.max(task[1] - task[0], remain - task[0]);
+12        }
+13        return ans;
+14    }
+15}
