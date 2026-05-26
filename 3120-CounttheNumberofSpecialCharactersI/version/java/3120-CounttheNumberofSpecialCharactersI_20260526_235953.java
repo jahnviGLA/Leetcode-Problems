1626b3051f56163e72ebaf898f531feff9e3002b
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-05-26 23:59:54 +0530

    Create 3120-CounttheNumberofSpecialCharactersI

---
+++ 3120-CounttheNumberofSpecialCharactersI/version/java/3120-CounttheNumberofSpecialCharactersI_20260526_235953.java
@@ -0,0 +1,13 @@
+// Last updated: 5/26/2026, 11:59:53 PM
+1class Solution {
+2    public int numberOfSpecialChars(String word) {
+3        int count = 0;
+4        for(char ch = 'a'; ch <= 'z'; ch++) {
+5            if(word.contains(String.valueOf(ch)) &&
+6               word.contains(String.valueOf(Character.toUpperCase(ch)))) {
+7                count++;
+8            }
+9        }
+10        return count;
+11    }
+12}
