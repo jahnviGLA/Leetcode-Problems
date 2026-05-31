7363f995e36724881215d91eda84cb05be002c3c
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-01 01:00:06 +0530

    Create

---
+++ problem-1780255806218/version/java/problem-1780255806218_20260601_010006.java
@@ -0,0 +1,15 @@
+// Last updated: 6/1/2026, 1:00:06 AM
+1import java.util.Arrays;
+2class Solution {
+3    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+4        Arrays.sort(asteroids);
+5        long currMass = mass; 
+6        for (int asteroid : asteroids) {
+7            if (currMass < asteroid) {
+8                return false;
+9            }
+10            currMass += asteroid;
+11        }
+12        return true;
+13    }
+14}
