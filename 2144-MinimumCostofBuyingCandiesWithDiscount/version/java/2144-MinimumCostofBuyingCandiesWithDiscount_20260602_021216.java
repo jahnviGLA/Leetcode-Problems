11c1c67bc8d11e24379898554d3d1b2c483ec0d7
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-02 02:12:17 +0530

    Create 2144-MinimumCostofBuyingCandiesWithDiscount

---
+++ 2144-MinimumCostofBuyingCandiesWithDiscount/version/java/2144-MinimumCostofBuyingCandiesWithDiscount_20260602_021216.java
@@ -0,0 +1,19 @@
+// Last updated: 6/2/2026, 2:12:16 AM
+1import java.util.*;
+2
+3class Solution {
+4    public int minimumCost(int[] cost) {
+5        Arrays.sort(cost);
+6        int ans = 0;
+7        int count = 0;
+8        for (int i = cost.length - 1; i >= 0; i--) {
+9            count++;
+10            if (count == 3) {
+11                count = 0;
+12                continue;
+13            }
+14            ans += cost[i];
+15        }
+16        return ans;
+17    }
+18}
