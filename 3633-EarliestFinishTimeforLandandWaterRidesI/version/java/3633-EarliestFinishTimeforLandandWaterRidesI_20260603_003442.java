414481a1f742c3c6b9c436c78290090ce150c8b1
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-03 00:34:43 +0530

    Create 3633-EarliestFinishTimeforLandandWaterRidesI

---
+++ 3633-EarliestFinishTimeforLandandWaterRidesI/version/java/3633-EarliestFinishTimeforLandandWaterRidesI_20260603_003442.java
@@ -0,0 +1,22 @@
+// Last updated: 6/3/2026, 12:34:42 AM
+1class Solution {
+2    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+3                                  int[] waterStartTime, int[] waterDuration) {       
+4        int ans = Integer.MAX_VALUE;        
+5        int n = landStartTime.length;
+6        int m = waterStartTime.length;
+7        for (int i = 0; i < n; i++) {
+8            for (int j = 0; j < m; j++) {
+9                int landFinish = landStartTime[i] + landDuration[i];
+10                int finishAfterWater = Math.max(landFinish, waterStartTime[j])
+11                                       + waterDuration[j];
+12                ans = Math.min(ans, finishAfterWater);
+13                int waterFinish = waterStartTime[j] + waterDuration[j];
+14                int finishAfterLand = Math.max(waterFinish, landStartTime[i])
+15                                      + landDuration[i];
+16                ans = Math.min(ans, finishAfterLand);
+17            }
+18        }
+19        return ans;
+20    }
+21}
