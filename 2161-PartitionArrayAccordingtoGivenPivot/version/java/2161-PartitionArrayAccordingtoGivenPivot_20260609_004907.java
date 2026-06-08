0b7d159df06b9e5e1566ad87bb843bfd8b495a67
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-09 00:49:08 +0530

    Create 2161-PartitionArrayAccordingtoGivenPivot

---
+++ 2161-PartitionArrayAccordingtoGivenPivot/version/java/2161-PartitionArrayAccordingtoGivenPivot_20260609_004907.java
@@ -0,0 +1,27 @@
+// Last updated: 6/9/2026, 12:49:07 AM
+1class Solution {
+2    public int[] pivotArray(int[] nums, int pivot) {
+3        int n = nums.length;
+4        int[] ans = new int[n];
+5        int index = 0;
+6        for(int num : nums) {
+7            if(num < pivot) {
+8                ans[index] = num;
+9                index++;
+10            }
+11        }
+12        for(int num : nums) {
+13            if(num == pivot) {
+14                ans[index] = num;
+15                index++;
+16            }
+17        }
+18        for(int num : nums) {
+19            if(num > pivot) {
+20                ans[index] = num;
+21                index++;
+22            }
+23        }
+24        return ans;
+25    }
+26}
