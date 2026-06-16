a8c844e553f78ee881b0e837422abed6a1149d30
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-16 20:05:31 +0530

    Create 3612-ProcessStringwithSpecialOperationsI

---
+++ 3612-ProcessStringwithSpecialOperationsI/version/java/3612-ProcessStringwithSpecialOperationsI_20260616_200535.java
@@ -0,0 +1,23 @@
+// Last updated: 6/16/2026, 8:05:35 PM
+1class Solution {
+2    public String processStr(String s) {
+3        StringBuilder result = new StringBuilder();
+4        for (char ch : s.toCharArray()) {
+5            if (ch >= 'a' && ch <= 'z') {
+6                result.append(ch);
+7            } 
+8            else if (ch == '*') {
+9                if (result.length() > 0) {
+10                    result.deleteCharAt(result.length() - 1);
+11                }
+12            } 
+13            else if (ch == '#') {
+14                result.append(result.toString());
+15            } 
+16            else if (ch == '%') {
+17                result.reverse();
+18            }
+19        }
+20        return result.toString();
+21    }
+22}
