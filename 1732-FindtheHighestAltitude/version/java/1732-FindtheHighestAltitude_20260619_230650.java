f99b137717f88fd6f511e3ba6709ce55c3f6891c
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-19 23:06:51 +0530

    Create 1732-FindtheHighestAltitude

---
+++ 1732-FindtheHighestAltitude/version/java/1732-FindtheHighestAltitude_20260619_230650.java
@@ -0,0 +1,12 @@
+// Last updated: 6/19/2026, 11:06:50 PM
+1class Solution {
+2    public int largestAltitude(int[] gain) {
+3        int currentAltitude = 0;
+4        int highestAltitude = 0;
+5        for(int altitudeGain : gain) {
+6            currentAltitude += altitudeGain;
+7            highestAltitude = Math.max(highestAltitude, currentAltitude);
+8        }
+9        return highestAltitude;
+10    }
+11}
