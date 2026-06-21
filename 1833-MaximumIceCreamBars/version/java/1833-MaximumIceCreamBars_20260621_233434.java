698a4dc5bdebe4ac821b9451fc2f6e669cd3a8d4
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-21 23:34:35 +0530

    Create 1833-MaximumIceCreamBars

---
+++ 1833-MaximumIceCreamBars/version/java/1833-MaximumIceCreamBars_20260621_233434.java
@@ -0,0 +1,25 @@
+// Last updated: 6/21/2026, 11:34:34 PM
+1class Solution {
+2    public int maxIceCream(int[] costs, int coins) {
+3        int maxCost = 0;
+4        for (int cost : costs) {
+5            maxCost = Math.max(maxCost, cost);
+6        }
+7        int[] freq = new int[maxCost + 1];
+8        for (int cost : costs) {
+9            freq[cost]++;
+10        }
+11        int iceCreams = 0;
+12        for (int price = 1; price <= maxCost; price++) {
+13            while (freq[price] > 0 && coins >= price) {
+14                coins -= price;
+15                iceCreams++;
+16                freq[price]--;
+17            }
+18            if (coins == 0) {
+19                break;
+20            }
+21        }
+22        return iceCreams;
+23    }
+24}
