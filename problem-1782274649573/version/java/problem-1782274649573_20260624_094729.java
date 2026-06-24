e6379cb74b332e8e0e72783f9ffe202a51ee2d4f
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-24 09:47:30 +0530

    Create

---
+++ problem-1782274649573/version/java/problem-1782274649573_20260624_094729.java
@@ -0,0 +1,17 @@
+// Last updated: 6/24/2026, 9:47:29 AM
+1class Solution {
+2    public int diagonalSum(int[][] mat) {
+3        int sum=0, j=mat.length-1;
+4        for(int i=0;i<mat.length;i++){
+5            sum+= mat[i][i];
+6            sum+=mat[i][j];
+7            j--;
+8        }
+9        if(mat.length%2==0){
+10            return sum;
+11        }
+12        else{
+13            return sum- mat[mat.length/2][mat.length/2];
+14        }
+15    }
+16}
