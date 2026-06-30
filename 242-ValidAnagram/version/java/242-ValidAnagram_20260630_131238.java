d11d6988c80c328de542d177fa6555a904bf285c
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-06-30 13:12:38 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/version/java/242-ValidAnagram_20260630_131238.java
@@ -0,0 +1,16 @@
+// Last updated: 6/30/2026, 1:12:38 PM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        if (s.length()!=t.length()) return false;
+4        int[] freq = new int[26];
+5        for (char c:s.toCharArray()) {
+6            freq[c-'a']++;
+7        }
+8        for (char c:t.toCharArray()) {
+9            freq[c-'a']--;
+10            if (freq[c-'a']<0) return false;
+11        }
+12        return true;
+13    }
+14}
+15
