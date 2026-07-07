ff1b85eed670532c27b16f705f933932f2d23569
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-07-07 13:47:11 +0530

    Create 3754-ConcatenateNon-ZeroDigitsandMultiplybySumI

---
+++ 3754-ConcatenateNon-ZeroDigitsandMultiplybySumI/version/java/3754-ConcatenateNon-ZeroDigitsandMultiplybySumI_20260707_134710.java
@@ -0,0 +1,22 @@
+// Last updated: 7/7/2026, 1:47:10 PM
+1class Solution {
+2    public long sumAndMultiply(int n) {
+3        int temp=0;
+4        int sum=0;
+5        while (n>0) {
+6            int r=n%10;
+7            sum +=r;
+8            if (r!=0) {
+9                temp=temp*10+r;
+10            }
+11            n/=10;
+12        }
+13        long rev=0;
+14        while (temp>0) {
+15            int rm=temp%10;
+16            rev=rev*10+rm;
+17            temp/=10;
+18        }
+19        return rev*sum;
+20    }
+21}
