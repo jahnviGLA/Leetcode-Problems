3fce32061fe06cd79d283d5fbdba97dc312ace09
Jahnvi Gupta <149038938+jahnviGLA@users.noreply.github.com>
2026-07-23 19:13:11 +0530

    Create 3513-NumberofUniqueXORTripletsI

---
+++ 3513-NumberofUniqueXORTripletsI/version/java/3513-NumberofUniqueXORTripletsI_20260723_191310.java
@@ -0,0 +1,11 @@
+// Last updated: 7/23/2026, 7:13:10 PM
+1class Solution {
+2    public int uniqueXorTriplets(int[] nums) {
+3        int n = nums.length;
+4
+5        if (n == 1) return 1;
+6        if (n == 2) return 2;
+7
+8        return 1 << (32 - Integer.numberOfLeadingZeros(n));
+9    }
+10}
